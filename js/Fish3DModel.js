0c5921ec2728c793d9ffac35db8296bad0dc35dc
Kenny Chung <kcmac@Kennys-MBP.home>
2018-09-23 09:45:13 -1000

    merge conflict

--- js/Fish3DModel.js
+++ js/Fish3DModel.js
@@ -2,63 +2,57 @@
 
 import React, { Component } from 'react';
 import { Viro3DObject, ViroNode, ViroText } from 'react-viro';
-import fishData from './fishy.json';
-const onClickEvent = function() {
-  this.setState({name : this.state.name ? "" : "TestName"});
-  this.setState({description : this.state.description ? "" : "TestDescription"});
-}
 
 class Fish3DModel extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.species = props.species;
-    this.state = {name : "", description : ""};
-  }
-  getInitialState() {
-    return {
-      name: "TestName",
-      description: "TestDescription"
-    };
+    this.handlePopUp = this.handlePopUp.bind(this);
   }
   handleEvent = () => {
     console.log(this.props);
   }
-  
 
-  
+  handlePopUp = () => {
+    this.props.position = [1, 1, 1]
+  }
+
+  onClickEvent = function() {
+    this.props.position = [1, 1, 1]
+  }
+
   render() {
     return (
-      <ViroNode onDrag = {this.props.onDrag || function() {}} dragType = {this.props.dragType} animation={this.props.animation}>
-        
+      <ViroNode onDrag={this.props.onDrag || function () { }} dragType={this.props.dragType} animation={this.props.animation}>
+
         <ViroText
-          text={this.state.name}
-          textLineBreakMode="Justify"
-          textClipMode="None"
-          color="#ff0000"
+          text={this.props.species}
+          color="red"
           width={2} height={2}
-          style={{fontFamily:"Arial", fontSize:20, fontStyle:"italic"}}
+          style={{ fontFamily: "Arial", fontSize: 20, fontStyle: "italic" }}
           position={[this.props.position[0], this.props.position[1] + 0.5, this.props.position[2]]}
         />
 
-        
+
         <ViroText
-          text={this.state.description}
-          textLineBreakMode="Justify"
-          textClipMode="None"
+          text="TestDescription"
+          textAlign="left"
+          textAlignVertical="top"
+          textLineBreakMode="justify"
+          textClipMode="clipToBounds"
           color="#ff0000"
           width={2} height={2}
-          style={{fontFamily:"Arial", fontSize:20, fontStyle:"italic"}}
+          style={{ fontFamily: "Arial", fontSize: 20, fontStyle: "italic" }}
           position={[this.props.position[0], this.props.position[1] - 0.5, this.props.position[2]]}
         />
 
         <Viro3DObject
-          source = {this.props.source}
-          type = {this.props.type}
-          position = {this.props.position}
-          scale = {this.props.scale}
-          rotation = {this.props.rotation}
-          onClick={onClickEvent.bind(this)}
-          
+          source={this.props.source}
+          type={this.props.type}
+          position={this.props.position}
+          scale={this.props.scale}
+          rotation={this.props.rotation}
+          onClick={() => {this.handlePopUp()}}
         />
       </ViroNode>
     );
