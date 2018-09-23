7a003fc49b7c7d53e4d3414d904f547e34fb239e
May Chen <maychen@Mays-MacBook-Pro.local>
2018-09-22 14:45:32 -1000

    Able to drag emoji

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,63 +1,86 @@
 'use strict';
 
 import React, { Component } from 'react';
-import {StyleSheet} from 'react-native';
+
+import { StyleSheet } from 'react-native';
+
 import {
   ViroARScene,
   ViroText,
-  ViroMaterials,
+  ViroConstants,
   ViroBox,
+  ViroMaterials,
   Viro3DObject,
   ViroAmbientLight,
   ViroSpotLight,
   ViroARPlaneSelector,
   ViroNode,
-  ViroAnimations,
 } from 'react-viro';
-var createReactClass = require('create-react-class');
-var HelloWorldSceneAR = createReactClass({
-  getInitialState() {
-    return {
-      text : "Initializing AR..."
-    };
-  },
 
-  render: function() {
+export default class HelloWorldSceneAR extends Component {
+
+  constructor() {
+    super();
+
+    // Set initial state here
+    this.state = {
+      text: "Initializing AR..."
+    };
+
+    // bind 'this' to functions
+    this._onInitialized = this._onInitialized.bind(this);
+  }
+
+  render() {
     return (
-      <ViroARScene onTrackingInitialized={()=>{this.setState({text : "Hello World!"})}}>
-        <ViroText text={this.state.text} scale={[.1, .1, .1]} height={1} width={4} position={[0, .5, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene onTrackingUpdated={this._onInitialized} >
+
+        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+
+        <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} materials={["grid"]} />
 
         <ViroAmbientLight color={"#aaaaaa"} />
-        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
-
-
+        <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
+          position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
+        <ViroNode position={[0, -1, 0]} dragType="FixedToWorld" onDrag={() => { }} >
           <Viro3DObject
-            source={require('./res/Magikarp/MagikarpF.vrx')}
-            position={[0, 0, -1]}
-            scale={[.002, .002, .002]}
-            rotation={[90, 90, 180]}
-            type="VRX"
-            dragType="FixedDistance" onDrag={()=>{}}
-            animation={{name:'animateImage', run:true}} 
-          />
+            source={require('./res/emoji_smile/emoji_smile.vrx')}
+            resources={[require('./res/emoji_smile/emoji_smile_diffuse.png'),
+            require('./res/emoji_smile/emoji_smile_normal.png'),
+            require('./res/emoji_smile/emoji_smile_specular.png')]}
+            position={[-.5, .5, -1]}
+            scale={[.2, .2, .2]}
+            type="VRX" />
+        </ViroNode>
 
       </ViroARScene>
     );
-  },
-});
+  }
 
-ViroAnimations.registerAnimations({
-  animateImage:{properties:{scaleX:.01, scaleY:.01, scaleZ:.01, opacity: 1},  
-        easing:"Bounce", duration: 5000},
-});
+  _onInitialized(state, reason) {
+    if (state == ViroConstants.TRACKING_NORMAL) {
+      this.setState({
+        text: "Hello World!"
+      });
+    } else if (state == ViroConstants.TRACKING_NONE) {
+      // Handle loss of tracking
+    }
+  }
+}
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
     fontFamily: 'Arial',
-    fontSize: 50,
+    fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
     textAlign: 'center',
+  },
+});
+
+ViroMaterials.createMaterials({
+  grid: {
+    diffuseTexture: require('./res/grid_bg.jpg'),
   },
 });
 
