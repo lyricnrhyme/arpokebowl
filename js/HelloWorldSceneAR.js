770888f817a2f869eab57eaa2c554e6406432093
Dakota <bazookaharwell@gmail.com>
2018-09-22 15:09:45 -1000

    Add dynamic spawning of fish using

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -14,11 +14,14 @@ import {
   ViroNode,
   ViroAnimations,
 } from 'react-viro';
+// TODO -
+const onClickEvent = function() { console.log("Hello world!"); }
 var createReactClass = require('create-react-class');
 var HelloWorldSceneAR = createReactClass({
   getInitialState() {
     return {
-      text : "Initializing AR..."
+      text : "Initializing AR...",
+      activeFish : [{},{},{}]
     };
   },
 
@@ -30,16 +33,23 @@ var HelloWorldSceneAR = createReactClass({
         <ViroAmbientLight color={"#aaaaaa"} />
         <ViroSpotLight innerAngle={5} outerAngle={90} direction={[0,-1,-.2]} position={[0, 3, 1]} color="#ffffff" castsShadow={true} />
 
+          // dynamic spawning of fish using arguments in this.state.activeFish
+          {
+            this.state.activeFish.map((item, i) => {
+              return (<Viro3DObject
+                source={require('./res/Magikarp/MagikarpF.vrx')}
+                position={[0, 0, -1 * i]}
+                scale={[.002, .002, .002]}
+                rotation={[90, 90, 180]}
+                type="VRX"
+                dragType="FixedDistance" onDrag={()=>{}}
+                animation={{name:'animateImage', run:true}}
+                onClick={() => console.log("hello world")}
+              />)
+            })
+          }
 
-          <Viro3DObject
-            source={require('./res/Magikarp/MagikarpF.vrx')}
-            position={[0, 0, -1]}
-            scale={[.002, .002, .002]}
-            rotation={[90, 90, 180]}
-            type="VRX"
-            dragType="FixedDistance" onDrag={()=>{}}
-            animation={{name:'animateImage', run:true}} 
-          />
+
 
       </ViroARScene>
     );
@@ -47,7 +57,7 @@ var HelloWorldSceneAR = createReactClass({
 });
 
 ViroAnimations.registerAnimations({
-  animateImage:{properties:{scaleX:.01, scaleY:.01, scaleZ:.01, opacity: 1},  
+  animateImage:{properties:{scaleX:.01, scaleY:.01, scaleZ:.01, opacity: 1},
         easing:"Bounce", duration: 5000},
 });
 
